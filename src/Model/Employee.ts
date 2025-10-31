b247561f25cdca513b6114b6674379ece80a95fc
Akanksha <akankshavaidya1999@gmail.com>
2025-10-31 15:48:15 +0530

    create employee reactiveform

---
+++ src/Model/Employee.ts
@@ -0,0 +1,22 @@
+export class EmployeeClass {
+    empId:number;
+    name:string;
+    city:string;
+    state:string;
+    emailId:string;
+    contactNo:string;
+    address:string;
+    pincode:string;
+
+    constructor(){
+        this.empId=0;
+        this.name='';
+        this.city ='';
+        this.state = '';
+        this.emailId='';
+        this.contactNo='';
+        this.address='';
+        this.pincode='';
+    
+}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeClass } from '../Model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_crud';
+  employeeForm: FormGroup = new FormGroup({});
+  employeeObj:EmployeeClass = new EmployeeClass();
+
+  constructor(){
+    this.createForm();
+  }
+
+  createForm(){
+    this.employeeForm=new FormGroup({
+      empid: new FormControl(this.employeeObj.empId),
+      name: new FormControl(this.employeeObj.name),
+      city: new FormControl(this.employeeObj.city),
+      address: new FormControl(this.employeeObj.address),
+      contactNo: new FormControl(this.employeeObj.contactNo),
+      emailId: new FormControl(this.employeeObj.emailId),
+      pincode: new FormControl(this.employeeObj.pincode),
+      state: new FormControl(this.employeeObj.state),
+    })
+  }
+
 }
